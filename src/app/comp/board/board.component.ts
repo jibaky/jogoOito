9cee9413e53087a603effa502593e43f9e07c199
jibaky <heitor_daniel@hotmail.com>
2023-10-19 14:29:07 -0300

    refinamento da terceira heuristica

--- src/app/comp/board/board.component.ts
+++ src/app/comp/board/board.component.ts
@@ -43,7 +43,7 @@ export class BoardComponent implements OnInit {
   }
 
   updateTiles(board: string[][]): void {
-    const side = 90;
+    const side = 105;
     for (let i = 0; i < board.length; i++) {
       for (let j = 0; j < board[i].length; j++) {
         const currentPiece = board[i][j] == '' ? 0 : board[i][j];
